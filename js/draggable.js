3baf75eb00d0968c4ede3f2eb1ea6f10c6b0e5bc
chloelizabeth <chloelizabethboyko@gmail.com>
2015-12-01 11:47:14 -0500

    add js file add draggable cant get to work

---
+++ js/draggable.js
@@ -0,0 +1,12 @@
+$(function(){
+
+$('.draggable').draggable();
+
+$('.resizable').resizable({
+    aspectRatio: true,
+    handles: 'ne, se, sw, nw'
+});
+
+$('.resizable').parent().rotatable();
+
+});
