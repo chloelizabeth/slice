b87a88eaccd18ac7607fbeff9e73757734166e90
chloelizabeth <chloelizabethboyko@gmail.com>
2015-12-16 00:40:26 -0500

    add constrain, z-index selector, and hide handles in js

--- js/draggable.js
+++ js/draggable.js
@@ -1,12 +1,42 @@
 $(function() {
 
 
-  $('.draggable').draggable();
+ var a = 10;
+    $('.collage1,.collage2,.collage3,.collage4,.collage5,.collage6').draggable({
+   start: function(event, ui) { $(this).css("z-index", a++); }
+});
+    $('#containment-wrapper').click(function() {
+        $(this).addClass('top').removeClass('bottom');
+        $(this).siblings().removeClass('top').addClass('bottom');
+        $(this).css("z-index", a++);
+
+    });
+
+
+
+ $('.draggable').draggable();
 
   $('.resizable').resizable({
       aspectRatio: true,
-      handles: 'ne, se, sw, nw'
+      handles: 'ne, se, sw, nw',
+      autoHide: true
+
+
   });
+   
+
+
+
+$('.draggable').draggable({ containment: "#containment-wrapper", scroll: false });
+
+ $( ".resizable" ).resizable({
+      containment: "#containment-wrapper", scroll: false
+    });
+
+
+
+
+
 
   //$('.resizable').parent().rotatable();
 
